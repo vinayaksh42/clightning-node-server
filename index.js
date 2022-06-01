40312319f27a12ff1b31dad04be6f7c453a8869d
vinayaksh42 <vinayaksh42@gmail.com>
2022-06-01 20:09:09 +0530

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./queries')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/channel_announcements', db.getChannelInfo)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
---
+++ queries.js
@@ -0,0 +1,21 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'historian_bitcoin',
+  password: 'password',
+  port: 5432,
+})
+
+const getChannelInfo = (request, response) => {
+    pool.query('SELECT * FROM "channel_announcements"', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+}
+
+module.exports = {
+    getChannelInfo
+  }
