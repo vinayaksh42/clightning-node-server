b758f0b435d7be61c5eb29832dd0c09174aa9896
vinayaksh42 <vinayaksh42@gmail.com>
2022-06-04 12:42:58 +0530

    postgres connection

--- queries.js
+++ queries.js
@@ -1,14 +1,19 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'historian_bitcoin',
-  password: 'password',
-  port: 5432,
-})
+var pg = require('pg');
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'historian_bitcoin',
+//   password: 'password',
+//   port: 5432,
+// })
+
+var conString = "postgres://postgres:password@localhost:5432/historian_bitcoin";
+
+var client = new pg.Client(conString);
+client.connect();
 
 const getChannelInfo = (request, response) => {
-    pool.query('SELECT * FROM "channel_announcements";', (error, results) => {
+    client.query('SELECT * FROM "channel_announcements";', (error, results) => {
       if (error) {
         throw error
       }
